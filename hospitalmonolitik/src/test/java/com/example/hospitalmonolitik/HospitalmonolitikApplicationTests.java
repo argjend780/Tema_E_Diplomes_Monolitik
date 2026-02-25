58704618c8864c76405515e010f4257fd7c004e1
argjend780 <am67266@ubt-uni.net>
2026-02-26 00:04:23 +0100

    Startimi i projektit me Spring boot

---
+++ hospitalmonolitik/src/test/java/com/example/hospitalmonolitik/HospitalmonolitikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalmonolitik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalmonolitikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
